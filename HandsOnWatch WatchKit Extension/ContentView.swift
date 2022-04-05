58fc7a8c0c714cca62fe0be39b6228c8d8764f86
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2022-04-05 16:12:59 -0300

    chore: added Color design tokens

--- HandsOnWatch WatchKit Extension/ContentView.swift
+++ HandsOnWatch WatchKit Extension/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import WatchKit
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 10) {
+                    VStack {
+                        Rectangle()
+                            .fill(DesignTokens.Colors.Brand.primaryColor)
+                            .cornerRadius(8)
+                            .frame(height: DesignTokens.Card.cardHeight)
+                            .padding(.horizontal, 5)
+                        
+                        Rectangle()
+                            .fill(DesignTokens.Colors.Support.primaryColor)
+                            .cornerRadius(8)
+                            .frame(height: DesignTokens.Card.cardHeight)
+                            .padding(.horizontal, 5)
+                        
+                        Rectangle()
+                            .fill(DesignTokens.Colors.Support.secondaryColor)
+                            .cornerRadius(8)
+                            .frame(height: DesignTokens.Card.cardHeight)
+                            .padding(.horizontal, 5)
+                    }
+                }
+            }
+            .background(.gray)
+        }
+        .navigationTitle("Flemis")
+        .navigationBarTitleDisplayMode(.inline)
+
     }
 }
 
---
+++ HandsOnWatch WatchKit Extension/Views/Components/Design-Tokens.swift
@@ -0,0 +1,88 @@
+//
+//  Design-Tokens.swift
+//  HandsOnWatch WatchKit Extension
+//
+//  Created by Marina De Pazzi on 05/04/22.
+//
+
+import Foundation
+import SwiftUI
+
+//MARK: - Hands On Design System
+enum DesignTokens {
+    
+    //MARK: - Colors
+    enum Colors {
+        
+        enum Brand {
+            
+            static var primaryColor: Color {
+                return Color(red: 0.471, green: 0.478, blue: 1)
+            }
+        }
+        
+        enum Support {
+            
+            static var primaryColor: Color {
+                return Color(red: 0.98, green: 0.067, blue: 0.31)
+            }
+            
+            static var secondaryColor: Color {
+                return Color(red: 0.016, green: 0.871, blue: 0.443)
+            }
+            
+            static var tertiaryColor: Color {
+                return Color(red: 1, green: 0.584, blue: 0)
+            }
+            
+            static var quaternaryColor: Color {
+                return Color(red: 0.353, green: 0.784, blue: 0.98)
+            }
+        }
+        
+        enum Neutral {
+            
+            static var lowPure: Color {
+                return Color(red: 0, green: 0, blue: 0)
+            }
+            
+            static var lowDark: Color {
+                return Color(red: 0.3, green: 0.3, blue: 0.3)
+            }
+            
+            static var lowLight: Color {
+                return Color(red: 0.608, green: 0.608, blue: 0.608)
+            }
+            
+            static var highPure: Color {
+                return Color(red: 1, green: 1, blue: 1)
+            }
+            
+            static var highDark: Color {
+                return Color(red: 0.75, green: 0.75, blue: 0.75)
+            }
+            
+            static var highLight: Color {
+                return Color(red: 0.871, green: 0.871, blue: 0.871)
+            }
+        }
+    }
+    
+    //MARK: - ComponentDimensions
+    
+    enum ComponentDimensions {
+        enum Card {
+            static var cardHeight: CGFloat = {
+                return WKInterfaceDevice.current().screenBounds.size.height * 0.36
+            }()
+        }
+    }
+    
+    //MARK: - Fonts
+    
+    //MARK: - Corner Radius
+    
+    //MARK: - Icons
+    
+    //MARK: - Opacity
+}
