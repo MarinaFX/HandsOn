17ec511ea62808708c5cc474f02ef83c088f7035
Lucas Dimer Justo <lucas.dimer.justo@hotmail.com>
2022-04-07 15:33:17 -0300

    Added category and time atributtes to Recipe.

--- HandsOnWatch WatchKit Extension/Model/Recipe.swift
+++ HandsOnWatch WatchKit Extension/Model/Recipe.swift
@@ -7,12 +7,25 @@
 
 import Foundation
 
+enum Category: CustomStringConvertible {
+    case risotto
+    
+    var description: String {
+        switch self {
+            case .risotto:
+                return "Risotto"
+        }
+    }
+}
+
 struct Recipe: Hashable {
     let title: String
     let produceQuantity: Double
     let produceUnitOfMeasurement: String
     var ingredients: [Ingredient]
     var instructions: [Instruction]
+    let category: Category
+    let time: String//should be in minutes
     
     static func == (lhs: Recipe, rhs: Recipe) -> Bool {
         return lhs.title == rhs.title
